c7e6bc3648918b14b0985d8e81799da6db750219
Noon <noon@Noons-MacBook-Pro.local>
2018-07-11 10:21:12 -0400

    update routes and import routes in  app.use in app.js

--- squad-diary/routes/entry.js
+++ squad-diary/routes/entry.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const entryController = require('../controllers/entry.js');
 
-router.get('/', )
-
+router.get('/index', entryController.index );
+router.get('/:weekNumber', entryController.show)
+router.post('/newEntry', entryController.createEntry);
+router.delete('/:id', entryController.deleteEntry);
 
 
 module.exports = router;
--- squad-diary/routes/index.js
+++ squad-diary/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-router.use('/', require('./entry'));
+router.use('/entry', require('./entry'));
+router.use('/topic', require('./lunchTopic'));
 
 
 module.exports = router;
