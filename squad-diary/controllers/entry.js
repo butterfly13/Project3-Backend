0fee6157a50e34d872e32b8ed002d00f167ffef1
Mariya <mariyawhitcomb@gmail.com>
2018-07-11 08:48:13 -0400

    fixed syntax

--- squad-diary/controllers/entry.js
+++ squad-diary/controllers/entry.js
@@ -1,49 +1,47 @@
-const Entry = require('../models/Entry')
+const Entry = require("../models/Entry");
 
 module.exports = {
-    // show all entry
-    index: (req, res) => {
-        Entry.find({})
-        .then(entry => {
-            res.json(entry)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    };
+  // show all entry
+  index: (req, res) => {
+    Entry.find({})
+      .then(entry => {
+        res.json(entry);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
 
-    // create new entry
-    createEntry: (req, res) => {
-        const weekNumber = req.body.weekNumber
-        const content = req.body.content
+  // create new entry
+  createEntry: (req, res) => {
+    const weekNumber = req.body.weekNumber;
+    const content = req.body.content;
 
-        Entry.create({
-            weekNumber: weekNumber,
-            content: content
-        })
-        .then(entry => {
-            res.json(entry)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    };
+    Entry.create({
+      weekNumber: weekNumber,
+      content: content
+    })
+      .then(entry => {
+        res.json(entry);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
 
-    // delete entry
-    deleteEntry: (req, res) => {
-        Entry.findByIdAndRemove(req.params.id)
-        .then(entry => {
-            if(!entry){
-                return res.status(404).send({
-                    message: 'Entry not find with id ' + req.params.id
-                })
-            }
-            res.send({message: 'Entry is deleted'})
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-
-
-}
-
+  // delete entry
+  deleteEntry: (req, res) => {
+    Entry.findByIdAndRemove(req.params.id)
+      .then(entry => {
+        if (!entry) {
+          return res.status(404).send({
+            message: "Entry not found with id " + req.params.id
+          });
+        }
+        res.send({ message: "Entry is deleted" });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
