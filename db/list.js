2398fde51216feba86704a7d49dcfe6a65ca53a6
Mariya <mariyawhitcomb@gmail.com>
2018-07-12 10:51:43 -0400

    added more seed data

--- db/list.js
+++ db/list.js
@@ -1,7 +1,8 @@
 module.exports = [
   {
-    weekNumber: 1,
-    content: "Week 1 was awesome",
+    weekNumber: 12,
+    content:
+      "You'll make it! Stick to it! Find a routine and don't sway from that. I was able to finish with a toddler so you have no exuses\nWDI21",
     dateCreated: {
       type: Date,
       default: Date.now()
@@ -9,47 +10,8 @@ module.exports = [
   },
   {
     weekNumber: 2,
-    content: "Week two was great",
-    dateCreated: {
-      type: Date,
-      default: Date.now()
-    }
-  },
-  {
-    weekNumber: 3,
-    content: "Project week",
-    dateCreated: {
-      type: Date,
-      default: Date.now()
-    }
-  },
-  {
-    weekNumber: 4,
-    content: "We are developers now we can do whatever we want",
-    dateCreated: {
-      type: Date,
-      default: Date.now()
-    }
-  },
-  {
-    weekNumber: 5,
-    content: "Everyone is confused",
-    dateCreated: {
-      type: Date,
-      default: Date.now()
-    }
-  },
-  {
-    weekNumber: 6,
-    content: "Half of the class did not make it to the second project",
-    dateCreated: {
-      type: Date,
-      default: Date.now()
-    }
-  },
-  {
-    weekNumber: 7,
-    content: "React is cool",
+    content:
+      "Welcome to GA! Tips:\n1. It is always fridgid here. Bring a jacket\n2. Don't forget to check-in before 9am\nWDI19",
     dateCreated: {
       type: Date,
       default: Date.now()
@@ -57,7 +19,52 @@ module.exports = [
   },
   {
     weekNumber: 8,
-    content: "Group project!!!",
+    content:
+      "Figure out a theme for your projects and let that inspire you. For me, I wanted a job in politics after GA, so my projects were all based on that.\nWDI20",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 4,
+    content:
+      "Keep it simple. It feels nice to be flashy but that's going to be a lot of work. Don't bite off more than you can chew\nWDI23",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 5,
+    content:
+      "Be sure to learn the basics. The basics are what makes a strong foundation. A weak base will have everything crushing down.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 6,
+    content:
+      "The road can get long and if you stop, you're going to stay there. Keep on going until you're out. Don't be discourage, everyone begins in ignorance.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 7,
+    content: "REACT IS AWESOME!!!!",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 8,
+    content:
+      "Stay on top of things - because the pace is brisk and every day brings a new challenge. Don't be afraid to speak up or ask for help when there's something you don't understand.\nWDI22",
     dateCreated: {
       type: Date,
       default: Date.now()
@@ -65,7 +72,179 @@ module.exports = [
   },
   {
     weekNumber: 9,
-    content: "We are not there yet",
+    content:
+      "Focus during project weeks and get shit done. It will seem like there is time to go back and make things right, but by the end you might not have the energy! Keep it simple and attainable when it comes to projects. YOU GOT THIS!\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 9,
+    content:
+      "You all have your own reasons for deciding to embark on this three month long journey. This journey is exciting. It is challenging. And trust me when I say it is rewarding!\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+
+  {
+    weekNumber: 1,
+    content:
+      "NEVER lose sight of your purpose and reason. When shit gets tough, always remind yourself: 'Why am I doing this?'\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 2,
+    content:
+      "Practice. Practice. Practice. Just jump in. Write that code. Create a mini project. Don't be afraid. You'll be amazed by how much you'll come to know\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 3,
+    content:
+      "Ask questions! Sometimes you may get excited, too excited that a code is working, but not necesserly understand why. Understand the WHYs too, not just the HOWs.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 4,
+    content:
+      "General Assembly is a great program that is well worth the cost. That said it is challenging and fast paced and you will eventually feel overwhelmed. But it is a part if the process! Don't lose hope. At the end of the program you will surprise yourself with how much you have learned. You will be a full stack developer.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 5,
+    content:
+      "Also learn to collaborate with your cohort. You guys are in it together and you will have a better experience overall if you build camraderie in your team.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 6,
+    content:
+      "Try! It does not matter if it works, if you get it, or if someone does it better.. Try and keep trying!\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 7,
+    content:
+      "Stick to it. It is going to SUCK! ALOT! There are days you will want to run. DON'T! Just stick to it. 12 weeks is nothing!\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 8,
+    content:
+      "Stay after class as much as possible\nAsk as many in-class questions as possible\nOn projects spend as much time as you can on the planning phase/pseudocode\nGoogle every problem before you go to the instructors for help.\nConsole.log is your friend\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 9,
+    content:
+      "You can do it! Hang in there, no matter what. There are good days and there are crazy bad days. That is normal. You aren't doing this wrong. You belong here.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 10,
+    content:
+      "Your hard work will pay off. It is so important to want the rest of your cohort to succeed as much as you want yourself to succeed. You as a group will make it bearable to get through WDI.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 11,
+    content:
+      "This thing you are about to do is going to be hard. And amazing. And soul crushing. And super rewarding.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 12,
+    content:
+      "Don't do it alone, your fellow students will help you through the tough times & celebrate with you in good. Your teachers got your back, reach out when you need it.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 1,
+    content:
+      "If you can't get past a problem I suggest you google it first, ask classmates that aren't busy second, and then go to the instructors. You'll eventually get good at searching for problems online and it will benefit you forever.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 10,
+    content:
+      "The course will teach you a lot you just have to keep on top of it. Make sure you take time after hours to increase your understanding of each topic before you move onto the next one. More effort you put in the more you'll understand.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 11,
+    content:
+      "This class is gonna challenge you a lot but it's totally worth it! Even though it's challenging it's pretty simple to make it through, ya just gotta do a few things: don't be afraid to ask for help, make friends with your classmates, try to not doubt yourself too much, try new things, don't overthink, and always try even if you don't exactly know how to do something! These 12 weeks will fly by for real so try and make the best of it. Good luck!\nWDI21",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 2,
+    content:
+      "You'll feel like you dont't know shit. But keep going. Don't focus on stupid things. Trust the hard work, process and grind. Maybe it will never be enough but it's all about perspective.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 12,
+    content:
+      "Time is about to stop but no worries it's only for three months. Get ready to accept what you don't know and be okay with it.\nWDI22",
+    dateCreated: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    weekNumber: 1,
+    content: "Programming is for everyone, not just the 'chosen'\nWDI22",
     dateCreated: {
       type: Date,
       default: Date.now()
