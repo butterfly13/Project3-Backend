a5f583adef942667b2197d91c0fdbc44966e3c58
Mariya <mariyawhitcomb@gmail.com>
2018-07-12 12:08:57 -0400

    fixed typos

--- db/topic.js
+++ db/topic.js
@@ -1,11 +1,38 @@
 module.exports = [
   {
-    content: "black mirror"
+    content: "Black Mirror: Episode 1"
   },
   {
-    content: "past present and future"
+    content: "Is past real or Earth came with pre-loaded memory?"
   },
   {
-    content: "immigration reform"
+    content: "Immigration reform"
+  },
+  {
+    content: "Social Media: PROs and CONs"
+  },
+  {
+    content: "What is present?"
+  },
+  {
+    content: "Political Correctness and Gender Identity"
+  },
+  {
+    content: "The existance of truth"
+  },
+  {
+    content: "Is Australia safe?"
+  },
+  {
+    content: "Best places to build St. Junipero"
+  },
+  {
+    content: "Whould you go to St.Junipero"
+  },
+  {
+    content: "Sporks vs. Forks"
+  },
+  {
+    content: "Straw usage issues"
   }
 ];
